e481e6f6828f27989e2ae64056fbb6629719125f
abdulmuneer <abdulmuneer@gmail.com>
2015-10-13 09:24:26 +0530

    basic view stub

--- technicolor_test/__init__.py
+++ technicolor_test/__init__.py
@@ -17,5 +17,8 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('user', '/user')
+    config.add_route('login', '/login')
+    config.add_route('status', '/status')
     config.scan()
     return config.make_wsgi_app()
--- technicolor_test/views.py
+++ technicolor_test/views.py
@@ -1,5 +1,7 @@
+import os
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.view import view_defaults
 
 from sqlalchemy.exc import DBAPIError
 
@@ -8,14 +10,51 @@ from .models import (
     MyModel,
     )
 
+@view_config(route_name='home', renderer='json')
+def system_status(request):
+    return {'database': 'ok'}
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'technicolor_test'}
+@view_config(route_name='login', renderer='json')
+def authenticate(request):
+    return {'key':'abcde12345'}
+
+
+
+
+@view_defaults(route_name='user')
+class USERView(object):
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(request_method='GET', renderer='json')
+    def get(self):
+        return Response('get')
+
+    @view_config(request_method='POST')
+    def post(self):
+        return Response('post')
+
+    @view_config(request_method='PUT')
+    def put(self):
+        return Response('put')
+
+    @view_config(request_method='DELETE')
+    def delete(self):
+        return Response('delete')
+
+@view_config(route_name='list', renderer='json')
+def listdir(request):
+    dir_name = request.matchdict['dir_path']
+    if os.path.isdir(dir_name):
+        return os.listdir(dir_name)
+
+# @view_config(route_name='home', renderer='templates/mytemplate.pt')
+# def my_view(request):
+#     try:
+#         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+#     except DBAPIError:
+#         return Response(conn_err_msg, content_type='text/plain', status_int=500)
+#     return {'one': one, 'project': 'technicolor_test'}
 
 
 conn_err_msg = """\
